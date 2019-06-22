0f2b914dc4d68feb95749b60df4689c8c1f05cfc
shaofeichang <chang1151230815@outlook.com>
2019-06-22 08:06:53 +0800

    Commit Launch.json

--- shelldir/demo.cpp
+++ shelldir/demo.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+//#include"head.h"
 using namespace std;
+extern void printHello();
 int main()
 {
+    printHello();
     cout<<"Helloworld"<<endl;
     cout<<"HelloWorld"<<endl;
     system("pause");
