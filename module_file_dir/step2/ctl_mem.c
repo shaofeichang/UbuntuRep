458ab2070876965dcbd4fc56bf14c07885ee8262
shaofeichang <chang1151230815@outlook.com>
2019-08-23 22:07:49 +0800

    ---

--- module_file_dir/step2/ctl_mem.c
+++ module_file_dir/step2/ctl_mem.c
@@ -5,23 +5,22 @@
 	> Created Time: Tue 28 Oct 2014 11:22:01 PM CST
  ************************************************************************/
 
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<sys/ioctl.h>
-#include"memdev.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include "memdev.h"
 
 int main(void)
 {
     int fd = 0;
     int test = 123;
 
-    fd = open("/dev/memdev0",O_RDWR);
+    fd = open("/dev/memdev0", O_RDWR);
 
-    ioctl(fd,MEM_RESTART);
+    ioctl(fd, MEM_RESTART);
     close(fd);
 
     return 0;
 }
-
