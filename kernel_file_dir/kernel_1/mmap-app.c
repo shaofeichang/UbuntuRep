b3bd7e2fd91d844d0af74b124578cb5bfad456bd
shaofeichang <chang1151230815@outlook.com>
2019-09-20 16:37:04 +0800

    readme

--- kernel_file_dir/kernel_1/mmap-app.c
+++ kernel_file_dir/kernel_1/mmap-app.c
@@ -10,22 +10,17 @@ int main()
     int fd;
     char *start;
     char buf[100];
-
     /*打开文件*/
     fd = open("testfile", O_RDWR);
     if(fd < 0)
         printf("open error\n");
     start = mmap(NULL, 100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
     /*读出数据*/
     strcpy(buf, start);
     printf("buf = %s\n", buf);
-
     /* 写入数据 */
     strcpy(start, "Buf Is Not Null!");
-
     munmap(start, 100); /* 解除映射 */
     close(fd);
-
     return 0;
 }
