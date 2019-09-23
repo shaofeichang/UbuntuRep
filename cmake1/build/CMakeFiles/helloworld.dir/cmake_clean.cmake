3abd87e93a448284515c3ca1f0fe8f84792a2b14
shaofeichang <chang1151230815@outlook.com>
2019-09-23 16:45:18 +0800

    cmake

--- cmake1/build/CMakeFiles/helloworld.dir/cmake_clean.cmake
+++ cmake1/build/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/helloworld.dir/helloworld.cpp.o"
   "helloworld.pdb"
   "helloworld"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
